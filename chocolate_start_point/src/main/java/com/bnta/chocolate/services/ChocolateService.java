b6a3c4ca8d2764bb985b203cdc920fee8af2ba55
blezzydcruz <mbannedelacruz@hotmail.com>
2023-08-08 16:42:35 +0100

    added chocolate and estate service

---
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/services/ChocolateService.java
@@ -0,0 +1,36 @@
+package com.bnta.chocolate.services;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.repositories.ChocolateRepository;
+import com.bnta.chocolate.repositories.EstateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ChocolateService {
+
+    @Autowired
+    ChocolateRepository chocolateRepository;
+
+    @Autowired
+    EstateRepository estateRepository;
+
+    public ChocolateService() {
+    }
+
+    public List<Chocolate> getAllChocolates() {
+        return chocolateRepository.findAll();
+    }
+
+    public Optional<Chocolate> getChocolateById(long id) {
+        return chocolateRepository.findById(id);
+    }
+
+    public Chocolate saveChocolate(Chocolate chocolate) {
+        chocolateRepository.save(chocolate);
+        return chocolate;
+    }
+}
